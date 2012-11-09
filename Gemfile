506aaf5bfac75269448dbdc916510a3692ba663d
Tony Beninate <tony@dealerignition.com>
2012-11-09 12:41:26 -0500

    Initial build with Locomotive.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'locomotive_cms', '~> 2.0.0.rc12', :require => 'locomotive/engine'
+gem 'rails', '3.2.8'
+
+group :assets do
+  gem 'coffee-rails',   '~> 3.2.2'
+  gem 'compass-rails',  '~> 1.0.2'
+  gem 'sass-rails',     '~> 3.2.4'
+  gem 'uglifier',       '~> 1.2.4'
+end
+
+group :development do
+  gem 'unicorn'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiMarketing::Application.config.secret_token = 'd68619b12ac3af55370ecb198430d2ad27c35ff52e9bf963a32356546aaa1fba817e4eea6dfdc702a38d5c1a5f07bbaf83d0650007c31ed1e6c477eb4a2763ca'
